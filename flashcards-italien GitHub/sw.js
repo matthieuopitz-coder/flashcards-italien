d57f67395f3ecbcd4a54d7eb453ff1664f46d209
matthieuopitz-coder <matthieu.opitz@gmail.com>
2026-06-10 11:09:02 +0200

    Add files via upload

---
+++ flashcards-italien GitHub/sw.js
@@ -0,0 +1,18 @@
+const CACHE = "flashcards-v4";
+const FILES = [
+  "/flashcards-italien/",
+  "/flashcards-italien/index.html",
+  "/flashcards-italien/manifest.json",
+  "/flashcards-italien/icon-192.png",
+  "/flashcards-italien/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
